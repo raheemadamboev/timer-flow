b7bcb920867eefc6bf85ec95d8165e9f95c60563
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-16 17:38:24 +0500

    add demo

--- app/src/main/java/xyz/teamgravity/timerflowdemo/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/timerflowdemo/MainActivity.kt
@@ -3,10 +3,19 @@ package xyz.teamgravity.timerflowdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import xyz.teamgravity.timer_flow.Timer
 import xyz.teamgravity.timerflowdemo.ui.theme.TimerFlowDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,6 +27,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val timer = remember {
+                        Timer().apply {
+                            timerDuration = 6_000L
+                            start()
+                        }
+                    }
+                    val time by timer.time.collectAsState()
+                    val progress by derivedStateOf { time / 6_000F }
+
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Text(text = (time.toString()))
+                        Spacer(modifier = Modifier.height(16.dp))
+                        LinearProgressIndicator(
+                            progress = progress,
+                            modifier = Modifier.fillMaxWidth(0.8F)
+                        )
+                    }
                 }
             }
         }
