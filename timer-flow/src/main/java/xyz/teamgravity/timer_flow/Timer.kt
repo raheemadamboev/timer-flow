0b9841d66cdc943d827cdb6f60f1fc2cac1320f7
raheem <raheemideadev@gmail.com>
2024-11-26 21:57:20 +0500

    [1.1.2] Upgraded dependencies, Kotlin, Gradle and targetSdk, compileSdk;

--- timer-flow/src/main/java/xyz/teamgravity/timer_flow/Timer.kt
+++ timer-flow/src/main/java/xyz/teamgravity/timer_flow/Timer.kt
@@ -1,21 +1,31 @@
 package xyz.teamgravity.timer_flow
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 class Timer {
 
-    companion object {
-        private const val DEFAULT_TIMER_DURATION = 10_000L
-        private const val DEFAULT_TIMER_CHECKPOINT = 3_000L
+    private companion object {
+        const val DEFAULT_TIMER_DURATION = 10_000L
+        const val DEFAULT_TIMER_CHECKPOINT = 3_000L
     }
 
     var timerDuration: Long = DEFAULT_TIMER_DURATION
     var timerCheckpoint: Long = DEFAULT_TIMER_CHECKPOINT
 
+    private val scope: CoroutineScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
     private var job: Job? = null
-    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
     private var sendCheckpoint = true
 
@@ -25,11 +35,55 @@ class Timer {
     private val _duration = MutableStateFlow(0L)
     val duration: StateFlow<Long> = _duration.asStateFlow()
 
-    private val _state = MutableStateFlow<TimerState>(TimerState.Idle)
+    private val _state = MutableStateFlow(TimerState.Idle)
     val state: StateFlow<TimerState> = _state.asStateFlow()
 
     private val _event = Channel<TimerEvent>()
     val event: Flow<TimerEvent> = _event.receiveAsFlow()
+
+    private fun startTimer() {
+        sendCheckpoint = true
+        job?.cancel()
+
+        job = scope.launch {
+            _time.emit(timerDuration)
+            _duration.emit(timerDuration)
+            _state.emit(TimerState.Ticking)
+            _event.trySend(TimerEvent.Started)
+
+            while (time.value > 0L) {
+                ensureActive()
+                delay(200L)
+
+                if (state.value == TimerState.Ticking) {
+                    _time.emit(time.value - 200L)
+
+                    if (time.value <= timerCheckpoint) {
+                        sendCheckpoint()
+                    }
+                }
+            }
+
+            finish()
+        }
+    }
+
+    private suspend fun finish() {
+        stop()
+        _state.emit(TimerState.Finished)
+        _event.trySend(TimerEvent.Finished)
+    }
+
+    private fun sendCheckpoint() {
+        if (sendCheckpoint) {
+            sendCheckpoint = false
+            _event.trySend(TimerEvent.Checkpoint)
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // API
+    ///////////////////////////////////////////////////////////////////////////
 
     fun start() {
         startTimer()
@@ -57,56 +111,21 @@ class Timer {
         job = null
     }
 
-    private fun startTimer() {
-        sendCheckpoint = true
-        job?.cancel()
+    ///////////////////////////////////////////////////////////////////////////
+    // MISC
+    ///////////////////////////////////////////////////////////////////////////
 
-        job = scope.launch {
-            _time.emit(timerDuration)
-            _duration.emit(timerDuration)
-            _state.emit(TimerState.Ticking)
-            _event.trySend(TimerEvent.Started)
-
-            while (_time.value > 0L) {
-                delay(200L)
-
-                if (_state.value == TimerState.Ticking) {
-                    _time.emit(_time.value - 200L)
-
-                    if (_time.value <= timerCheckpoint) {
-                        sendCheckpoint()
-                    }
-                }
-            }
-
-            finish()
-        }
+    enum class TimerState {
+        Idle, // timer ready to start
+        Ticking, // timer is ticking
+        Paused, // timer in paused state
+        Finished, // timer finished
+        Stopped; // timer stopped programmatically
     }
 
-    private suspend fun finish() {
-        stop()
-        _state.emit(TimerState.Finished)
-        _event.trySend(TimerEvent.Finished)
-    }
-
-    private fun sendCheckpoint() {
-        if (sendCheckpoint) {
-            sendCheckpoint = false
-            _event.trySend(TimerEvent.Checkpoint)
-        }
-    }
-
-    sealed class TimerState {
-        object Idle : TimerState() // timer ready to start
-        object Ticking : TimerState() // timer is ticking
-        object Paused : TimerState() // timer in paused state
-        object Finished : TimerState() // timer finished
-        object Stopped : TimerState() // timer stopped programmatically
-    }
-
-    sealed class TimerEvent {
-        object Started : TimerEvent()
-        object Checkpoint : TimerEvent()
-        object Finished : TimerEvent()
+    enum class TimerEvent {
+        Started,
+        Checkpoint,
+        Finished;
     }
 }
